6b83ef1e234c641d4420f1e2d84e8a6ba78e2feb
unknown <anazambrana79@gmail.com>
2025-07-25 20:35:47 -0300

    mi proyecto final

---
+++ clase7.py.py
@@ -0,0 +1,17 @@
+
+DNI=47905742
+print(DNI)
+DNI=23118660
+print(DNI)
+
+nombre=input("ingrese su nombre")
+print("Bienvenido", nombre)
+
+numero1=int(input("ingresar un numero entero"))
+numero2=int(input("ingresar un numero entero"))
+numero3=int(input("ingresar un numero entero"))
+
+suma=numero1+numero2+numero3
+resultado=suma//3
+
+print("el promedio de los tres numeros es", resultado)
